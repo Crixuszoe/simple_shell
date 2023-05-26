ebf6e378d6128626ccde70c8fc31438c2677ba0b
Crixuszoe <supersoftfirm@gmail.com>
2023-05-26 07:58:30 -0700

    Variables in shell_loop, modified

--- shell_loop.c
+++ shell_loop.c
@@ -68,7 +68,7 @@ int find_builtin(info_t *info)
 	};
 
 	for (x = 0; builtintbl[x].type; x++)
-		if (_strcmp(info->argv[0], builtintbl[i].type) == 0)
+		if (_strcmp(info->argv[0], builtintbl[x].type) == 0)
 		{
 			info->line_count++;
 			built_in_ret = builtintbl[i].func(info);
